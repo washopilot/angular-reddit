a188d5b4ed6e02d82a9a80c5ce86419b9a8afb05
Fernando Chicaiza <washopilot@yahoo.com>
2022-03-25 09:21:28 -0500

    encapsulando modelo y refactorizando

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,5 @@
 import { Component, HostBinding, OnInit } from '@angular/core';
+import { Article } from './article.model';
 
 @Component({
   selector: 'app-article',
@@ -7,19 +8,20 @@ import { Component, HostBinding, OnInit } from '@angular/core';
 })
 export class ArticleComponent implements OnInit {
   @HostBinding('attr.class') cssClass = 'row';
-  votes: number;
-  title: string;
-  link: string;
+  article: Article;
 
   constructor() {
-    (this.title = 'Angular'), (this.link = 'http://angular.io'), (this.votes = 10);
+    this.article = new Article('Angular', 'http://angular.io', 10);
   }
 
-  voteUp() {
-    this.votes += 1;
+  voteUp(): boolean {
+    this.article.voteUp();
+    return false;
   }
-  voteDown() {
-    this.votes -= 1;
+  voteDown(): boolean {
+    this.article.voteDown();
+    return false;
   }
+
   ngOnInit(): void {}
 }
